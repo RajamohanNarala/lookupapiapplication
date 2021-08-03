2af2754c41026b8157e9775e0ac2e09f801d9668
NaralaR <Rajamohan.Narala.HCL@kantar.com>
2021-08-03 09:25:34 +0530

    Incorporated changes as per JIRA comments.

--- src/test/java/com/kantar/lookup/application/lookupapplication/LookupapplicationApplicationTests.java
+++ src/test/java/com/kantar/lookup/application/lookupapplication/LookupapplicationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LookupapplicationApplicationTests {
+class PanelistManagermentServiceTests {
 
 	@Test
 	void contextLoads() {
